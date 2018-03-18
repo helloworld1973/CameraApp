367e16676a2ff7f2eee2b6e1ed7053766774370b
helloworld1973 <yxzhjason@gmail.com>
2018-03-18 15:47:24 +1300

    Initial commit

---
+++ CameraApp/CameraApp.Android/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("CameraApp.Droid.MainApplication, CameraApp.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md52d409cb1694d17f91b302696456d112e.MainApplication.class, md52d409cb1694d17f91b302696456d112e.MainApplication.__md_methods);
+		
+	}
+}
